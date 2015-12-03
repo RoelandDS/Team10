017ffbb7c9154f9183ebc7c39dabfc0e32ade239
Simon De Boeck <simondeboeck1@hotmail.com>
2015-12-03 14:14:23 +0100

    Json

--- htdocs/routes/index.js
+++ htdocs/routes/index.js
@@ -1,9 +1,10 @@
 var express = require('express');
+
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.json([{"name":"test","members": [{"name":"test"},{"name":"test2"}]}]);
 });
 
 module.exports = router;
