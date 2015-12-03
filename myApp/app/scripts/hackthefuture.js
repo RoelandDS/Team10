0c24d4a6999ee21de9e80f5616439b4a987cb640
Jelle Van den Steen <jelle-vdst@live.be>
2015-12-03 13:27:26 +0100

    refactor

--- myApp/app/scripts/hackthefuture.js
+++ myApp/app/scripts/hackthefuture.js
@@ -1,5 +1,25 @@
 var app = angular.module('hackthefuture', ['ui.router','ui.bootstrap']);
 
+app.config([
+    '$stateProvider',
+    '$urlRouterProvider',
+    function($stateProvider,$urlRouterProvider){
+        $stateProvider.state('lamp', {
+            url: '/lamp',
+            templateUrl: '/kleur.html',
+            controller: 'hueController'
+        }).state('register', {
+            url: '/register',
+            templateUrl: '/register.html',
+            controller: 'hueController'
+        });
+
+        $urlRouterProvider.otherwise('registreerController');    
+    }]);
+
+
+
+
 
 
 
